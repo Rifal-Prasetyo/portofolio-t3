8d01981a6cb9cfe2b867060797be67204f4cc48f
rifal-prasetyo <hrdsdms765@gmail.com>
2025-05-08 21:02:29 +0700

    add: project section & contact section

--- src/features/home/pages/Homepage.tsx
+++ src/features/home/pages/Homepage.tsx
@@ -372,7 +372,7 @@ const Home = () => {
                     </div>
                 </div>
             </section>
-            <section className='w-full mt-2' id='project'>
+            <section className='w-full mt-2 h-auto pt-4 md:w-full container transition-all duration-500 ease-in-out pb-4 overflow-auto border-b border-primary dark:border-primary-dark' id='project'>
                 <header className="w-full">
                     <h1 className="text-lg md:text-xl font-bold flex items-center gap-2">
                         <Package size={20} />
@@ -415,6 +415,60 @@ const Home = () => {
                             ))
                         }
                     </div>
+                </header>
+            </section>
+            <section className='w-full h-auto pt-4 md:w-full container transition-all duration-500 ease-in-out pb-4 overflow-auto border-b border-primary dark:border-primary-dark mt-2' id='article'>
+                <header className="w-full">
+                    <h1 className="text-lg md:text-xl font-bold flex items-center gap-2">
+                        <Article size={20} />
+                        {t('Article.title')}
+                    </h1>
+                    <h2 className="text-sm md:text-lg ">
+                        {t('Article.subtitle')}
+                    </h2>
+                    <div className='w-full grid grid-cols-1 md:grid-cols-2 gap-2 mt-2'>
+                        {
+                            [
+                                {
+                                    image: '/assets/img/articles/img1.jpg',
+                                    title: 'Aku NPC bisa apa',
+                                    excerpt: 'Katanya masa SMA paling banyak kenangannya, tapi tidak disaya. dahlah :(',
+                                    slug: 'aku_npc_bisa_apa',
+                                },
+                                {
+                                    image: '/assets/img/articles/img2.jpg',
+                                    title: 'Ngedit Dulu',
+                                    excerpt: 'Katanya masa SMA paling banyak kenangannya, tapi tidak disaya. dahlah :(',
+                                    slug: 'ngedit_dulu',
+                                }
+                            ].map((item, index) => (
+                                <div className='flex p-2 gap-3 items-center overflow-hidden rounded-md border border-dashed border-primary dark:border-primary-dark' key={index}>
+                                    <Image src={item.image} alt={item.title} width={100} height={100} className='aspect-square object-cover rounded-md'/>
+                                    <div className='flex flex-col '>
+                                        <h5 className='font-bold text-sm md:text-lg'>{item.title}</h5>
+                                        <p className='font-light text-xs md:text-sm '>
+                                            {item.excerpt}
+                                        </p>
+                                        <Link  href={"/articles" + item.slug} className='flex items-center gap-2 p-2 rounded-md border border-dashed border-primary dark:border-primary-dark text-primary dark:text-primary-dark hover:opacity-80'>
+                                                <ArrowBendUpRight size={20}/>
+                                                {t('Article.link')}
+                                        </Link>
+                                    </div>
+                                </div>
+                            ))
+                        }
+                    </div>
+                </header>
+            </section>
+            <section className='w-full h-auto pt-4 md:w-full container transition-all duration-500 ease-in-out pb-4 overflow-auto border-b border-primary dark:border-primary-dark mt-2' id='article'>
+                <header className="w-full">
+                    <h1 className="text-lg md:text-xl font-bold flex items-center gap-2">
+                        <AddressBook size={20} />
+                        {t('Contact.title')}
+                    </h1>
+                    <h2 className="text-sm md:text-lg ">
+                        {t('Contact.subtitle')}
+                    </h2>
                 </header>
             </section>
         </div>
